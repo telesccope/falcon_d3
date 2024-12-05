950684d13b8b9913b2196fab7978b4d42d074b08
telesccope <884664869@qq.com>
2024-12-05 12:35:31 +0000

    feat: using local host in script.js

--- frontend/script.js
+++ frontend/script.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
         .attr("height", height);
 
     const axiosInstance = axios.create({
-        baseURL: 'http://127.0.0.1:5000' 
+        baseURL: 'http://localhost:5000' 
     });
 
     // 获取第一个图的数据
